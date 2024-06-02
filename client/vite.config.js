5e6c18be3ef5bbd6ebb39174d11a2622ff19bef1
Faiza Maliat <122308040+FaizaM07@users.noreply.github.com>
2024-06-02 17:39:33 +0600

    edited vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:3000',
-        secure:false,
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 1,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+      webp: {
+        quality: 75,
+      },
+    }),
+  ],
+  assetsInclude: ["**/*.WEBP"],
 });
